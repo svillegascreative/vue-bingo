596a9f5b9dc570ba5fd8edc7fbd3f152251e07cf
sylvia villegas <sylvia@svillegas.com>
2019-07-18 11:29:15 -0400

    Add basic styles and content to box component

--- src/components/box.vue
+++ src/components/box.vue
@@ -1,12 +1,23 @@
 <template>
-  <div></div>
+  <div class="game-box">{{boxContent}}</div>
 </template>
 
 <script>
 export default {
-  name: "GameBox"
+  name: "GameBox",
+  props: {
+    boxContent: String
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+.game-box {
+  height: 100px;
+  width: 100px;
+  border: 1px solid;
+  display: table-cell;
+  vertical-align: middle;
+  text-align: center;
+}
 </style>
