66496ae3c69958c8b5471a4138afffc18d4f8b15
sylvia villegas <sylvia@svillegas.com>
2019-07-19 11:22:59 -0400

    Add basic board structure

--- src/components/board.vue
+++ src/components/board.vue
@@ -1,12 +1,25 @@
 <template>
-  <div></div>
+  <div class="game-board">
+    <GameBox v-for="box in gameBoxes" :key="box.boxContent"></GameBox>
+  </div>
 </template>
 
 <script>
+import GameBox from "./box.vue";
+
 export default {
-  name: "GameBoard"
+  name: "GameBoard",
+  components: {
+    GameBox
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+.game-board {
+  border: 1px solid;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+}
 </style>
