1f6e05aee31d4ac1aace1cdcb7f209a5dfcb8886
sylvia villegas <sylvia@svillegas.com>
2019-07-19 14:44:22 -0400

    Move test data to own file

---
+++ src/data/boxes.js
@@ -0,0 +1,12 @@
+const testBoxes = [
+	{ id: 1, text: 'box 1' },
+	{ id: 2, text: 'box 2' },
+	{ id: 3, text: 'box 3' },
+	{ id: 4, text: 'box 4' },
+	{ id: 5, text: 'box 5' },
+	{ id: 6, text: 'box 6' },
+	{ id: 7, text: 'box 7' },
+	{ id: 8, text: 'box 8' },
+	{ id: 9, text: 'box 9' },
+];
+export default testBoxes;
