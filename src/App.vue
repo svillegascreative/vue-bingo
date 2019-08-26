9f57699c3c5fdb2d864fb82fd901cf021c3fa550
sylvia villegas <sylvia@svillegas.com>
2019-08-26 10:57:22 -0400

    Create GameStatus component
    - GameStatus receives boxesPlayed from App whenever GameBoard changes

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
   <div id="app">
-    <GameBoard :boxes="boxes"></GameBoard>
+    <GameBoard :boxes="boxes" @board-change="updateGameStatus"></GameBoard>
+    <GameStatus :boxesPlayed="boxesPlayed"></GameStatus>
   </div>
 </template>
 
 <script>
-import GameBoard from "./components/board.vue";
+import GameBoard from "@/components/board.vue";
+import GameStatus from "@/components/game-status.vue";
 import testBoxes from "@/data/boxes.js";
 
 export default {
   name: "app",
   components: {
-    GameBoard
+    GameBoard,
+    GameStatus
   },
   data() {
     return {
-      boxes: testBoxes
+      boxes: testBoxes,
+      boxesPlayed: []
     };
+  },
+  methods: {
+    updateGameStatus: function(val) {
+      this.boxesPlayed = val;
+    }
   }
 };
 </script>
---
+++ src/components/game-status.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="resetGame">Reset Game</button>
+</template>
+
+<script>
+export default {
+  name: "GameStatus",
+  props: {
+    initialGame: Array,
+    boxesPlayed: Array
+  },
+  data() {
+    return {
+      currentGame: []
+    };
+  },
+  watch: {
+    boxesPlayed: function() {
+      console.log("played: " + this.boxesPlayed);
+    }
+  },
+  methods: {
+    resetGame: function() {},
+    checkForWin: function() {}
+  }
+};
+</script>
+
+<style>
+</style>
